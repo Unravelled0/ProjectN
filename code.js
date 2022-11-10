11cf4f58ea50be29ca74af7997202caca5f98451
Unravelled0 <namazdervosov@mail.ru>
2022-11-11 01:05:11 +0400

    worked on input part

--- code.js
+++ code.js
@@ -1,5 +1,12 @@
 const taskInput = document.querySelector(".task-input input");
-taskBox = document.querySelector(".task-box");
+
+const actionButton = document.querySelector('.action');
+
+
+
+taskBox = document.querySelector(".value_list");
+
+const deleteButton = document.querySelector('.delete')
 
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
@@ -7,29 +14,68 @@ function showTodo() {
     let li = "";
     if(todos){
     todos.forEach((todo,id) => {
-        li +=  ` <li class="task">
+        li +=  ` <li class="value_list">
         <label for="${id}">
             <p>${todo.name}</p>
         </label>
-        <div class="delete">
-            <img class="delete-icon" src="photos\jpg2.svg" alt="as">
-        </div>
+
+        <button class="delete" onclick=''> </button>
+        
     </li>`;
     });
         }
     taskBox.innerHTML = li;
 }
+
 showTodo();
-taskInput.addEventListener("keyup", e => {
+
+function deleteTask(deleteId){
+    console.log('delete')
+    todos.splice(deleteId,1);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo();
+}
+
+
+deleteButton.addEventListener('click',deleteTask);
+ actionButton.addEventListener('click',addFunction);
+ function addFunction() {
     let userTask = taskInput.value.trim();
-    if(e.key == "Enter" && userTask){
-        if(!todos){
-            todos = [];
-        }
-        taskInput.value = "";
+    if(!todos){
+        todos = [];
+     }
+    if(userTask) {
         let taskInfo = {name: userTask, status:"pending"};
         todos.push(taskInfo);
+        taskInput.value = "";
         localStorage.setItem("todo-list", JSON.stringify(todos));
         showTodo();
     }
+  }
+
+// taskInput.addEventListener("keyup", e => {
+//     let userTask = taskInput.value.trim();
+//     if(e.key == "Enter" && userTask){
+//         if(!todos){
+//             todos = [];
+//         }
+//         taskInput.value = "";
+//         let taskInfo = {name: userTask, status:"pending"};
+//         todos.push(taskInfo);
+//         localStorage.setItem("todo-list", JSON.stringify(todos));
+//         showTodo();
+//     }
+// });
+
+todoListElement === null || todoListElement === void 0 ? void 0 : todoListElement.addEventListener("click", (event) => {
+    var _a;
+    const target = event.target;
+    if (target.className.includes('delete')) {
+        // (event.target as HTMLElement).parentElement?.remove();
+        (_a = target.closest('li')) === null || _a === void 0 ? void 0 : _a.remove();
+        updateTheListoCounter();
+    }
 });
+
+
+  
