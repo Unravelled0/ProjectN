df44e9292fc55640ee444fd64aa295687d18cddf
Unravelled0 <namazdervosov@mail.ru>
2022-11-13 11:54:58 +0400

    done css and html part endly

--- code.js
+++ code.js
@@ -1,81 +1,118 @@
-const taskInput = document.querySelector(".task-input input");
-
-const actionButton = document.querySelector('.action');
 
 
+let listdiv = document.querySelector('.value_list');
+new Sortable(listdiv);
 
-taskBox = document.querySelector(".value_list");
+let listarray = [];
+let pic1 = document.querySelector('.arrows');
 
-const deleteButton = document.querySelector('.delete')
+pic1.addEventListener('mouseover', ()=>{
+    if(pic1.src == 'http://127.0.0.1:5500/Photos/lowToHighGrey.svg')
+    pic1.src = 'Photos/HighToLowblack.svg';
+    else if(pic1.src == 'http://127.0.0.1:5500/Photos/HighToLowgrey.svg')
+    pic1.src = 'Photos/lowToHighBlack.svg';
+});
 
-let todos = JSON.parse(localStorage.getItem("todo-list"));
+pic1.addEventListener('mouseout',() =>{
+    if(pic1.src == 'http://127.0.0.1:5500/Photos/HighToLowblack.svg')
+    pic1.src = 'Photos/lowToHighGrey.svg';
+    else if(pic1.src == 'http://127.0.0.1:5500/Photos/lowToHighBlack.svg')
+    pic1.src = 'Photos/HighToLowgrey.svg';
+});
 
-function showTodo() {
-    let li = "";
-    if(todos){
-    todos.forEach((todo,id) => {
-        li +=  ` <li class="value_list">
-        <label for="${id}">
-            <p>${todo.name}</p>
-        </label>
 
-        <button class="delete" onclick=''> </button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let todos = JSON.parse(localStorage.getItem("todo-list"));
+
+// function showTodo() {
+//     let li = "";
+//     if(todos){
+//     todos.forEach((todo,id) => {
+//         li +=  ` <li class="value_list">
+//         <label for="${id}">
+//             <p>${todo.name}</p>
+//         </label>
+
+//         <button class="delete" onclick=''> </button>
         
-    </li>`;
-    });
-        }
-    taskBox.innerHTML = li;
-}
-
-showTodo();
-
-function deleteTask(deleteId){
-    console.log('delete')
-    todos.splice(deleteId,1);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo();
-}
-
-
-deleteButton.addEventListener('click',deleteTask);
- actionButton.addEventListener('click',addFunction);
- function addFunction() {
-    let userTask = taskInput.value.trim();
-    if(!todos){
-        todos = [];
-     }
-    if(userTask) {
-        let taskInfo = {name: userTask, status:"pending"};
-        todos.push(taskInfo);
-        taskInput.value = "";
-        localStorage.setItem("todo-list", JSON.stringify(todos));
-        showTodo();
-    }
-  }
-
-// taskInput.addEventListener("keyup", e => {
-//     let userTask = taskInput.value.trim();
-//     if(e.key == "Enter" && userTask){
-//         if(!todos){
-//             todos = [];
+//     </li>`;
+//     });
 //         }
-//         taskInput.value = "";
+//     taskBox.innerHTML = li;
+// }
+
+// showTodo();
+
+// function deleteTask(deleteId){
+//     console.log('delete')
+//     todos.splice(deleteId,1);
+//     localStorage.setItem("todo-list", JSON.stringify(todos));
+//     showTodo();
+// }
+
+
+// deleteButton.addEventListener('click',deleteTask);
+//  actionButton.addEventListener('click',addFunction);
+//  function addFunction() {
+//     let userTask = taskInput.value.trim();
+//     if(!todos){
+//         todos = [];
+//      }
+//     if(userTask) {
 //         let taskInfo = {name: userTask, status:"pending"};
 //         todos.push(taskInfo);
+//         taskInput.value = "";
 //         localStorage.setItem("todo-list", JSON.stringify(todos));
 //         showTodo();
 //     }
-// });
+//   }
 
-todoListElement === null || todoListElement === void 0 ? void 0 : todoListElement.addEventListener("click", (event) => {
-    var _a;
-    const target = event.target;
-    if (target.className.includes('delete')) {
-        // (event.target as HTMLElement).parentElement?.remove();
-        (_a = target.closest('li')) === null || _a === void 0 ? void 0 : _a.remove();
-        updateTheListoCounter();
-    }
-});
+// // taskInput.addEventListener("keyup", e => {
+// //     let userTask = taskInput.value.trim();
+// //     if(e.key == "Enter" && userTask){
+// //         if(!todos){
+// //             todos = [];
+// //         }
+// //         taskInput.value = "";
+// //         let taskInfo = {name: userTask, status:"pending"};
+// //         todos.push(taskInfo);
+// //         localStorage.setItem("todo-list", JSON.stringify(todos));
+// //         showTodo();
+// //     }
+// // });
+
+// todoListElement === null || todoListElement === void 0 ? void 0 : todoListElement.addEventListener("click", (event) => {
+//     var _a;
+//     const target = event.target;
+//     if (target.className.includes('delete')) {
+//         // (event.target as HTMLElement).parentElement?.remove();
+//         (_a = target.closest('li')) === null || _a === void 0 ? void 0 : _a.remove();
+//         updateTheListoCounter();
+//     }
+// });
 
 
   
